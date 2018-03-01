a43d9470650d6765105362602d89da64c68d682d
gerard naughton <gerard naughton>
2018-03-01 15:24:36 +0000

    Task 3 code complete

--- Task2/README.md
+++ Task2/README.md
@@ -5,13 +5,25 @@
 ## Objective
 Provided from our Lecturer Ian Mcloughlin
 
-<p><img src="Images/task1.PNG" width="500" length="300"></p>
+<p><img src="Images/task2.PNG" width="500" length="300"></p>
 
 ## Algorithm
 
 
 
 ## Solution
+
+```
+(define (collatz-list n);
+  (if (= n 1)
+      (cons n true)
+      (cons n(if (integer? (/ n 2)
+          (collatz-list (/ n 2))
+          (collatz-list (+ 1 (* n 3)))))
+      )
+  )
+
+```
 
 ## Conclusion
 
---
+++ Task3/task3-lcycle-rcycle.rkt
@@ -0,0 +1,69 @@
+#lang racket
+
+;Gerard Naughton G00209309 Theory of Algotithms
+;Task 3
+
+;Write, from scratch, two functions in Racket. The first is called lcycle. It takes a
+;list as input and returns the list cyclically shifted one place to the left. The second
+;is called rcycle, and it shifts the list cyclically shifted one place to the right.
+
+;lcycle function that shifts the list cyclically one place to the left
+(define (lcycle l); define function name and parameter
+  (if (null? l); if null return empty list
+      '()
+      (append (cdr l) ;get the 2nd element onward of the list l
+              (cons (car l) ;get first element in l and create a list
+                    '()))));which we will append to the previous list 
+
+; Function removes last element in a list Adapted from: https://stackoverflow.com/questions/5006750/removing-last-element-of-a-listscheme
+(define (removeLast l);define function name and parameter
+    (if (null? (cdr l));if next element in list is null
+        '() 
+        (cons (car l) (removeLast (cdr l))))) ;otherwise keep adding till list
+
+; rcycle function that shifts the list cyclically one place to the right
+; Uses removeLast method which uses more basic functions belonging to racket
+(define (rcycle l); define function name and parameter 
+  (if (null? l) ;if null return empty list
+      '()
+      (append (cons (last l) ; Last function gives last element in list
+              (removeLast l); call removeLast function which return list with last element removed and then appends rest of list to the last element
+                    ))))
+
+;rcycle2 function that shifts the list cyclically one place to the right
+;This function uses the reverse function twice to remove the last element of the list 
+; and doesnt require the removeLast function
+(define (rcycle2 l); define function name and parameter 
+  (if (null? l);if null return empty list
+      '()
+      (append (cons (last l) ; Last function gives last element in list
+              (reverse(cdr(reverse l)))); removes last from list then apends the rest of list to the last element
+                    )))
+
+
+
+;Testing
+
+;define 2 lists
+(define l(list 1 2 3 4 5))
+(define l2 (list 10 20 50 100 120))
+
+;lcycle
+(lcycle l)
+(lcycle l2)
+
+;rcycle
+(rcycle l)
+(rcycle l2)
+
+;rcycle2
+(rcycle2 l)
+(rcycle2 l2)
+
+;Results should show:
+;'(2 3 4 5 1)
+;'(20 50 100 120 10)
+;'(5 1 2 3 4)
+;'(120 10 20 50 100)
+;'(5 1 2 3 4)
+;'(120 10 20 50 100)
